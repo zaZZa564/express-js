7011ccfc890ac5a4cd5d87b744b092303f58c1dd
zaZZa564 <anus_bobra564@mail.ru>
2025-04-19 19:55:01 +0300

    dotenv file

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import { twitRouter } from './src/twit/twit.controller.js';
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 const app = express();
 
@@ -12,7 +15,7 @@ async function main() {
     res.status(404).json({message: 'Not found'})
   })
 
-  app.listen(3000, () => {
+  app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running on port 3000');
   })
 }
