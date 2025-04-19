64d61d304892e05e28f1fe720a75b53df981f769
zaZZa564 <anus_bobra564@mail.ru>
2025-04-19 19:44:19 +0300

    vaidation of data

--- src/twit/twit.controller.js
+++ src/twit/twit.controller.js
@@ -6,6 +6,10 @@ const router = Router()
 const twistService = new TwitService()
 
 router.post('/', (req, res) => {
+  // валидация
+  if(req.body.text.length < 1) {
+    return res.status(404).json({message: "message required"})
+  }
   const twit = twistService.createTwit(req.body)
   res.status(200).json(twit)
 })
