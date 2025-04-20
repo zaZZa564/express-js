cdb28ff65d71e32b71773f3aae8f7679ff45b9a1
zaZZa564 <anus_bobra564@mail.ru>
2025-04-20 20:36:13 +0300

    request body

--- src/twit/twit.controller.js
+++ src/twit/twit.controller.js
@@ -10,7 +10,9 @@ router.post('/', (req, res) => {
   if(req.body?.text?.length < 1) {
     return res.status(404).json({message: "message required"})
   }
-  
+
+  console.log(req.headers)
+
   const twit = twistService.createTwit(req.body)
   res.status(200).json(twit)
 })
